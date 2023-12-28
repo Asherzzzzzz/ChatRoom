8736960797db54ca26899229911421ead0340f63
Asherzzzzzz <onepiece12365@gmail.com>
2023-12-28 10:01:43 +0800

    update ClientMain.cpp

--- ChatRoomClient/ClientMain.cpp
+++ ChatRoomClient/ClientMain.cpp
@@ -9,6 +9,11 @@ using namespace std;
 #define SERVER_PORT 9909
 #define RECONNECT_WAIT_MS 5000
 
+#define ACCOUNT_MAX_SIZE 50
+#define PASSWORD_MAX_SIZE 50
+#define LOGIN_PACKET_MAX_SIZE 6 + ACCOUNT_MAX_SIZE + PASSWORD_MAX_SIZE
+
+
 enum packetId
 {
 	checkStatus,
@@ -30,6 +35,42 @@ enum status
 char to_char(int n)
 {
 	return '0' + n;
+}
+
+bool sentToServer(SOCKET s, const char* buff, int buffLen)
+{
+	int hasSent = 0;
+	do
+	{
+		hasSent = send(s, buff + hasSent, buffLen - hasSent, 0);
+		if (hasSent == SOCKET_ERROR)
+		{
+			cerr << "send() failed with error: " << WSAGetLastError() << endl;
+			return false;
+		}
+	} while (hasSent != buffLen);
+
+	return true;
+}
+
+int toLoginPacket(char* account, char* password, char* outputPacket)
+{
+	//packet_id + account + accountSize + password + passwordSize + \0
+	if (outputPacket)
+	{
+		outputPacket[0] = to_char(login);
+		outputPacket[1] = to_char(strlen(account) / 10);
+		outputPacket[2] = to_char(strlen(account) % 10);
+		strcat_s(outputPacket, 106, account);
+		outputPacket[strlen(account) + 3] = to_char(strlen(password) / 10);
+		outputPacket[strlen(account) + 4] = to_char(strlen(password) % 10);
+		outputPacket[strlen(account) + 5] = '\0';
+		strcat_s(outputPacket, 106, password);
+
+		return strlen(outputPacket);
+	}
+
+	return -1;
 }
 
 
@@ -77,29 +118,24 @@ int main()
 		}
 
 		
-		char packetBuff[106] = { 0 }; //packet_id + account + accountSize + password + passwordSize + endingChar
-		char account[50], password[50];
+		char* packetBuff = new char[LOGIN_PACKET_MAX_SIZE] { 0 }, //packet_id + account + accountSize + password + passwordSize + endingChar
+			* account = new char[ACCOUNT_MAX_SIZE] { 0 }, 
+			* password = new char[PASSWORD_MAX_SIZE] { 0 };
 		
 		cout << "Account: ";
-		cin.getline(account, 50);
+		cin.getline(account, ACCOUNT_MAX_SIZE);
 		cout << "Password: ";
-		cin.getline(password, 50);
+		cin.getline(password, PASSWORD_MAX_SIZE);
 
-		packetBuff[0] = to_char(login);
-		packetBuff[1] = to_char(strlen(account) / 10);
-		packetBuff[2] = to_char(strlen(account) % 10);
-		strcat_s(packetBuff, 106, account);
-		packetBuff[strlen(account) + 3] = to_char(strlen(password) / 10);
-		packetBuff[strlen(account) + 4] = to_char(strlen(password) % 10);
-		packetBuff[strlen(account) + 5] = '\0';
-		strcat_s(packetBuff, 106, password);
-
-		returnValue = send(clientSocket, packetBuff, strlen(packetBuff), 0);
-		if (returnValue == SOCKET_ERROR)
+		int packetLen = toLoginPacket(account, password, packetBuff);
+		
+		if (!sentToServer(clientSocket, packetBuff, packetLen))
 		{
-			cerr << "send() failed with error: " << WSAGetLastError() << endl;
-			
+			cerr << "Reconnecting..." << endl;
+			Sleep(RECONNECT_WAIT_MS);
+			continue;
 		}
+		
 	}
 	
 
